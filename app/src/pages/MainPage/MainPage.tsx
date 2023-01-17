db92c59b87e3c0343668b9a063c59b3603cf3209
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-01-17 22:51:20 +0100

    feat: add ModalWindow to MainPage

--- app/src/pages/MainPage/MainPage.tsx
+++ app/src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,5 @@
 import {
-  Box,
   Container,
-  Modal,
   Table,
   TableBody,
   TableCell,
@@ -9,41 +7,19 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import ModalWindow from "components/ModalWindow/ModalWindow";
 import ProductCard from "components/ProductCard/ProductCard";
-import React, { useEffect, useState } from "react";
-import { IProduct, IResponse } from "types/types";
-
-const style = {
-  position: "absolute" as const,
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import { getAllProducts } from "features/product.slice";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import React, { useEffect } from "react";
 
 const MainPage = () => {
-  const [products, setProducts] = useState<IProduct[]>();
-  const [count, setCount] = useState<number>();
-  const [open, setOpen] = useState<boolean>(false);
+  const { products, amountPages } = useAppSelector((state) => state.product);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    console.log("hi");
-    fetch("https://reqres.in/api/products")
-      .then((res) => res.json())
-      .then((data: IResponse) => {
-        setProducts(data.data);
-        setCount(data.total_pages);
-      })
-      .catch((error) => console.log(error));
+    dispatch(getAllProducts());
   }, []);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
     <Container
@@ -56,7 +32,7 @@ const MainPage = () => {
       }}
     >
       <Typography variant="h2" sx={{ mt: 2, mb: 3 }}>
-        Products {count}
+        Products
       </Typography>
       <Table
         sx={{ width: "60%", boxShadow: "0px 0px 10px 0.5px #b8b6b6", mb: 3 }}
@@ -81,16 +57,7 @@ const MainPage = () => {
           })}
         </TableBody>
       </Table>
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+      <ModalWindow />
     </Container>
   );
 };
