fb07db15fda2c1de2cf1a11bcf2e595774441ace
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-01-17 22:03:56 +0100

    feat: create store

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,16 @@
 import Layout from "./components/Layout/Layout";
 import React from "react";
 import MainPage from "pages/MainPage/MainPage";
+import { Provider } from "react-redux";
+import { store } from "store/store";
 
 function App() {
   return (
-    <Layout>
-      <MainPage />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <MainPage />
+      </Layout>
+    </Provider>
   );
 }
 
