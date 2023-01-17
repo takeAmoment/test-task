82f4aa97243da81cf026e1b6e4d7b52ac6f08ae2
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-01-17 22:49:15 +0100

    feat: add setProduct to productCard

--- app/src/components/ProductCard/ProductCard.tsx
+++ app/src/components/ProductCard/ProductCard.tsx
@@ -1,10 +1,19 @@
 import { TableCell, TableRow } from "@mui/material";
+import { setProduct } from "features/product.slice";
+import { useAppDispatch } from "hooks/redux";
 import React, { FC } from "react";
 import { IProductCardProps } from "types/types";
 
 const ProductCard: FC<IProductCardProps> = ({ item }) => {
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(setProduct(item));
+  };
   return (
-    <TableRow sx={{ backgroundColor: `${item.color}`, cursor: "pointer" }}>
+    <TableRow
+      sx={{ backgroundColor: `${item.color}`, cursor: "pointer" }}
+      onClick={handleClick}
+    >
       <TableCell align="center">{item.id}</TableCell>
       <TableCell align="center">{item.name}</TableCell>
       <TableCell align="center">{item.year}</TableCell>
