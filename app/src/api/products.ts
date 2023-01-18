91be0f694a2acf7bdc7719aad3518be24a238bb7
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-01-18 12:52:53 +0100

    fix: fix getAllProductsApi request

--- app/src/api/products.ts
+++ app/src/api/products.ts
@@ -3,6 +3,6 @@ import { IProductResponse, IResponse } from "types/types";
 const baseUrl = "https://reqres.in/api/products";
 
 export const getAllProductsApi = async (page: number) =>
-  await axios.get<IResponse>(baseUrl + `?page=${page}`);
+  await axios.get<IResponse>(baseUrl + `?page=${page}&per_page=5`);
 export const getProductByIdApi = async (id: number) =>
   await axios.get<IProductResponse>(baseUrl + `?id=${id}`);
