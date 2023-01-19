677bacf0bfad16d02680b3565e2e7e8742b83698
Aliaksandra Piakhota <sasha@macbook-air-aliaksandra.home>
2023-01-19 22:41:46 +0100

    feat: change BrowserRouter to HashRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,17 +3,17 @@ import React from "react";
 import MainPage from "pages/MainPage/MainPage";
 import { Provider } from "react-redux";
 import { setupStore } from "store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={setupStore()}>
         <Layout>
           <MainPage />
         </Layout>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
